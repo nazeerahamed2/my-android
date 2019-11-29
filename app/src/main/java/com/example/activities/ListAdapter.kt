de7ffa096dd2700bd97dcfe744d8577d4848a28a
Nazeer Ahamed <nazeer@tmmmt.sa>
2019-11-29 18:16:16 +0530

    Fixed dialog issues

--- app/src/main/java/com/example/activities/ListAdapter.kt
+++ app/src/main/java/com/example/activities/ListAdapter.kt
@@ -10,6 +10,8 @@ import kotlinx.android.synthetic.main.model_track_itesm.view.*
 class ListAdapter(context: Context, var activitiesList: MutableList<ActivitiesDbModel>) :
     androidx.recyclerview.widget.RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
 
+    private var isDialogShown: Boolean = false
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder =
         ListViewHolder(parent.inflate(R.layout.model_track_itesm))
 
@@ -44,9 +46,9 @@ class ListAdapter(context: Context, var activitiesList: MutableList<ActivitiesDb
                     uiIvIcon.setColorFilter(ContextCompat.getColor(context, R.color.colorBlue))
                 }
             }
-            if (getReminderTimeFromDb() != 0.toLong()) {
-                    if (System.currentTimeMillis() > getReminderTimeFromDb())
-                        showReminderDialog(context)
+            if (activityItem.name == context.getString(R.string.str_name_hydration)) {
+                    if (getReminderTimeFromDb() != 0.toLong() && System.currentTimeMillis() > getReminderTimeFromDb())
+                        if(!isDialogShown) showReminderDialog(context)
                 }
         }
     }
@@ -55,11 +57,13 @@ class ListAdapter(context: Context, var activitiesList: MutableList<ActivitiesDb
         val dialogBuilder = AlertDialog.Builder(context)
         dialogBuilder.setMessage(context.getString(R.string.str_remind_hydration))
             .setCancelable(false)
-            .setPositiveButton("Ok") { dialog, id -> dialog.cancel()
+            .setPositiveButton("Ok") { dialog, id ->
+                dialog.cancel()
             }
         val alert = dialogBuilder.create()
         alert.setTitle(context.getString(R.string.str_reminder))
         alert.show()
+        isDialogShown = true
     }
 
     inner class ListViewHolder(view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view)
